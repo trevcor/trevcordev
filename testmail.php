282eafe4b6b89ff78ed4ee22a91a45e441696a7d
trevcor <tfcorcor@gmail.com>
2013-01-31 14:12:20 -0500

    remove testmail.php

--- testmail.php
+++
@@ -1,9 +0,0 @@
-<?php
-$efrom = "From: tfcorcor@gmail.com\r\n";
-mail(
-'tfcorcor@gmail.com', // your email address
-'Testing', // email subject
-'This is an email sent from MAMP', // email body
-$efrom . "\r\n"// additional headers
-);
-?>
